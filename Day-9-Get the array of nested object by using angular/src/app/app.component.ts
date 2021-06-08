f8973737271c6446e6ea5b30f8b21e6844a47754
sooriya08 <83271390+sooriya08@users.noreply.github.com>
2021-06-08 22:05:34 +0530

    Add files via upload

---
+++ Day-9-Get the array of nested object by using angular/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { StudentsComponent } from './students/students.component';
+
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Template-form';
+
+
+//   semester: Array<any>=["sem1","sem2","sem3","sem4"]
+
+  
+
+//  onSubmit(data: any){
+//    console.log(data);
+//  }
+
+ 
+ 
+
+
+}
