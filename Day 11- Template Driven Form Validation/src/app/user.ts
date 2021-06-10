7e59dcd92319fbe091b9e81e15d39c2b6b5f31c1
sooriya08 <83271390+sooriya08@users.noreply.github.com>
2021-06-10 19:47:52 +0530

    Add files via upload

---
+++ Day 11- Template Driven Form Validation/src/app/user.ts
@@ -0,0 +1,11 @@
+export class user {
+
+    constructor(
+        public name: any,
+        public password: any,
+        public email: any,
+        public Address: any
+        
+      ) {  }
+
+}
